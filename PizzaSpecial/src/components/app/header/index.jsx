6ebebe1e3594ff31f4ddaf894ceff960f77abe91
Maol <martin.ollinen@gmail.com>
2023-10-16 22:14:01 +0200

    first commit

---
+++ PizzaSpecial/src/components/app/header/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Helmet} from 'react-helmet'
+const Header = () => {
+    const menuList = ['Menu',];
+
+    return (
+        <div >
+        <Helmet>
+            TEST Header
+    
+        </Helmet>
+        </div>
+    );
+};
+
+export default Header;
